5f4aee6eda4e81c666e0070dcb6669544a6454f5
Kamil Smołucha <kamsmo10074@gmail.com>
2019-03-10 22:20:41 +0100

    Baza

---
+++ Dziennik_internetowy/Dziennik_internetowy/Models/Uczniowie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dziennik_internetowy.Models
+{
+    public class Uczniowie
+    {
+        public int IdUcznia { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Miejsce_zamieszkania { get; set; }
+        public int Klasa { get; set; }
+    }
+}
---
+++ Dziennik_internetowy/SzkolaDB/dbo/Tables/Uczniowie.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Uczniowie]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
